20b5dd3c49a8a02c97f3a0e2da42d1e457a07b60
Tanvir Ahmad <tanvirahmadstudent@gmail.com>
2023-05-19 19:03:24 +0600

    implement delete API for specific Toy delete

--- index.js
+++ index.js
@@ -44,6 +44,12 @@ async function run() {
       const result = await toyCollection.insertOne(newToy);
       res.send(result);
     })
+    app.delete('/toy/:id',async(req,res)=>{
+       const id = req.params.id;
+       const query = {_id : new ObjectId(id)};
+       const result = await toyCollection.deleteOne(query);
+       res.send(result);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
