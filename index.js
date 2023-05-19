768b1acf8cb4832755f9245e75df492000a14dca
Tanvir Ahmad <tanvirahmadstudent@gmail.com>
2023-05-19 09:33:20 +0600

    added environment file for hide username and password

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const cors    = require('cors');
+require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app     = express();
+
 const port    = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://ToyManager:sPKiCJn7qmfktQvf@cluster0.xpliwro.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xpliwro.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
