db834ec87060d7a323e19233d19f84232095d50b
Tanvir Ahmad <tanvirahmadstudent@gmail.com>
2023-05-19 17:03:14 +0600

    create API for specific Single Toy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require('dotenv').config()
 const cors    = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app     = express();
 const port    = process.env.PORT || 5000;
 
@@ -25,9 +25,15 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const toyCollection = client.db('toyDB').collection('toys');
-    app.get('/all-toy',async(req,res)=>{
+    app.get('/toys',async(req,res)=>{
        const result = await toyCollection.find().toArray();
        res.send(result);
+    })
+    app.get("/toy/:id",async(req,res)=>{
+       const id = req.params.id;
+       const query = {_id: new ObjectId(id)};
+       const result = await toyCollection.findOne(query);
+       res.send(result); 
     })
     app.post('/add-toy',async(req,res)=>{
       const newToy = req.body;
